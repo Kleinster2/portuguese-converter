8e664066a0476e1ddb2566c268d4b807ec0cae49
Kleinster2 <69709492+Kleinster2@users.noreply.github.com>
2025-01-30 03:56:01 -0500

    Switch to BaseHTTPRequestHandler

--- api/index.py
+++ api/index.py
@@ -1,8 +1,9 @@
-def handler(request):
-    return {
-        'statusCode': 200,
-        'headers': {
-            'Content-Type': 'application/json'
-        },
-        'body': '{"message": "Hello from Python!"}'
-    }
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        message = '{"message": "Hello from Python!"}'
+        self.wfile.write(message.encode())
