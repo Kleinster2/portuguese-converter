1cb401e9affe98cef3d554852485392b038849d9
Kleinster2 <69709492+Kleinster2@users.noreply.github.com>
2025-01-30 05:32:26 -0500

    Switch to Flask

--- api/index.py
+++ api/index.py
@@ -1,5 +1,7 @@
-def handler(request):
-    return {
-        'statusCode': 200,
-        'body': 'Hello from Python!'
-    }
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({'message': 'Hello from Python!'})
